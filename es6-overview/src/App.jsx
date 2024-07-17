de99696ce2442e278164a341994fe90a121220c8
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-07-17 10:52:19 +0600

    project

---
+++ es6-overview/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Sunglass from './components/Sunglass/Sunglass'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Sunglass />
+
+      <h1>emtiaz</h1>
+    </>
+  )
+}
+
+export default App
---
+++ es6-overview/src/components/Sunglass/Sunglass.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Sunglass() {
+  return (
+    <div>Sunglass</div>
+  )
+}
+
+export default Sunglass
